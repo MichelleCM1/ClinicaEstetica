831cb9b2c27883da9d2e30d67b64978882cc5860
MICHELLE CAROLINE MARTINS DOS SANTOS <michelle.cmsantos1@senacsp.edu.br>
2024-04-18 21:54:36 -0300

    continuação

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -2,20 +2,47 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [nome, setNome] = useState('');
+  const [idade, setIdade] = useState('');
+  const [email, setEmail] = useState('');
+  const [historico, setHistorico] =useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    console.log('Dados do paciente:', {nome, idade, email, historico});
+
+    setNome('');
+    setIdade('');
+    setEmail('');
+    setHistorico('');
+  };
 
   return (
     <>
       <div>
+      <h1>Cadastro do paciente</h1>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Nome:
+          <input type="text" value ={nome} onChange={(e) => setNome(e.target.value)} />
+        </label>
+        <label>
+          Email:
+          <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+        </label>
+        <label>
+          Historico:
+          <textarea value={historico} onChange={(e) => setHistorico(e.target.value)} />
+        </label>
+        <button type="submit"> Cadastrar</button>
+
+
+
+
+      </form>
+      </div>
      
-      </div>
-      <h1>Histórico</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-      </div>
-  
     </>
   )
 }
